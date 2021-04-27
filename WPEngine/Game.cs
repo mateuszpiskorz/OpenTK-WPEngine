74430bb2561b928d3436d1052f372d3c72459437
piskorzm <piskorzm@hotmail.com>
2021-04-27 18:37:25 +0200

    Dodaj pliki projektów.

---
+++ WPEngine/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Mathematics;
+
+namespace WPEngine
+{
+    public class Game : GameWindow
+    {
+        private Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+           
+        }
+
+        public static Game Initialize(int width, int height, double renderFrequency, string title)
+        {
+            GameWindowSettings defaultGWS = GameWindowSettings.Default;
+            NativeWindowSettings defaultNWS = NativeWindowSettings.Default;
+
+            defaultGWS.RenderFrequency = renderFrequency;
+
+            defaultNWS.Size = new Vector2i(width, height);
+            defaultNWS.Title = title;
+
+            return new Game(defaultGWS, defaultNWS);
+        }
+    }
+}
---
+++ WPEngine/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WPEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game game = Game.Initialize(800, 600, 60.0, "OpenTKTest"))
+            {
+                game.Run();
+            }
+        }
+    }
+}
